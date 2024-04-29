eaf5b234dcd828622444e1a1f1083cc93914ea58
raherygino <georgino197@gmail.com>
2024-04-29 20:48:58 +0300

    Add file needly

---
+++ app/models/db/database.py
@@ -0,0 +1,9 @@
+import sqlite3
+import os
+
+class Database():
+    def connect(self):
+        folder_path = os.path.expanduser("~/AppData/Local/gdc/")
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        return sqlite3.connect(f"{folder_path}database.db")
