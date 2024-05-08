c5c36d0a0af3139679ec2b19c414628c85f6f346
raherygino <georgino197@gmail.com>
2024-05-08 21:59:32 +0300

    Delete item

--- app/presenter/depot_presenter.py
+++ app/presenter/depot_presenter.py
@@ -1,5 +1,8 @@
-from PyQt5.QtCore import QThread
+from PyQt5.QtCore import QThread, QPoint
+from PyQt5.QtGui import QCursor
 from ..models import DatabaseWorker
+from .menu_presenter import MenuAction
+from qfluentwidgets import RoundMenu, Action, FluentIcon, MenuAnimationType
 
 class DepotPresenter:
     
@@ -16,8 +19,25 @@ class DepotPresenter:
         self.__initWidget()
         
     def __initWidget(self):
-        self.headerLabel = ["Date","Rubriques","Types", "Marques", "Modele", "Nombre", "Accessoires", "Etat", "Fonctionnalité", "Motif", "Observation", ""]
+        self.headerLabel = ["ID","Date","Rubriques","Types", "Marques", "Modele", "Nombre", "Accessoires", "Etat", "Fonctionnalité", "Motif", "Observation", ""]
         self.view.tableView.setHorizontalHeaderLabels(self.headerLabel)
+        self.view.tableView.contextMenuEvent = lambda event: self.mouseRightClick(event)
+    
+    def mouseRightClick(self, event):
+        selectedItems = self.view.tableView.selectedItems()
+        if (len(selectedItems) != 0):
+            idItem = self.view.tableView.selectedItems()[0].text()
+            action = MenuAction(self)
+            menu = RoundMenu(parent=self.view)
+            #menu.addAction(Action(FluentIcon.FOLDER, 'Voir', triggered = lambda:action.show(matricule_item)))
+            menu.addAction(Action(FluentIcon.EDIT, 'Modifier', triggered = lambda: action.update(idItem)))
+            menu.addSeparator()
+            menu.addAction(Action(FluentIcon.DELETE, 'Supprimer', triggered = lambda: action.confirmDelete(idItem)))
+
+            self.posCur = QCursor().pos()
+            cur_x = self.posCur.x()
+            cur_y = self.posCur.y()
+            menu.exec(QPoint(cur_x, cur_y), aniType=MenuAnimationType.FADE_IN_DROP_DOWN)
     
     def fetchData(self, data):
         self.view.progressBar.setVisible(True)
@@ -45,7 +65,7 @@ class DepotPresenter:
         listData = []
         listData.clear()
         for material in data:
-            listData.append([material.date, material.name, 
+            listData.append([material.id, material.date, material.name, 
                              material.type, material.brand, 
                              material.model, material.count, 
                              material.accessory, material.state,  
